908046cbcf2a274a3f31e8b7e274a879b88987a3
Enrico Russo <enrico.russo@unige.it>
2020-09-27 18:39:54 +0200

    Update admin url (nax)

--- attacker/app.py
+++ attacker/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request
 import json, base64
 
+admin_path = "/nax"
+
 HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
 
 class attackerFlask(Flask):
     def process_response(self, response):
         attacker = loadAttacker(request.path[1:])
 
-        if request.path[0:6] != '/admin':
+        if request.path[0:len(admin_path)] != admin_path:
             if 'header' in attacker:
                 if attacker['header']:
                     for header in  attacker['header'].split('\n'):
@@ -29,7 +31,7 @@ def loadAttacker(path):
     else:
         fname = None
 
-    print(fname)
+    print("Loading: {} ({})".format(fname, path))
     try:
         with open(fname, "r") as f:
             attacker = json.loads(f.read())
@@ -42,7 +44,7 @@ def loadAttacker(path):
 
     return attacker
 
-@app.route("/admin/update", methods=['POST'])
+@app.route(admin_path + "/update", methods=['POST'])
 def update():
     code = request.values.get('code')
     msg = request.values.get('msg')
@@ -57,45 +59,35 @@ def update():
     else:
         fname = "json/attacker.json"
 
+    print("Saving: " + fname)
+
     with open(fname, "w") as f:
         f.write(json.dumps(attacker))
     f.close()
 
     return "{}"
 
-@app.route('/admin', defaults={'path': ''})
-@app.route('/admin/', defaults={'path': ''})
-@app.route('/admin/<path:path>', methods=['GET', 'POST'])
+@app.route(admin_path, defaults={'path': ''})
+@app.route(admin_path + '/', defaults={'path': ''})
+@app.route(admin_path + '/<path:path>', methods=['GET', 'POST'])
 def admin(path):
     attacker = loadAttacker(path)
 
-    return render_template('admin.html', attacker=attacker)
+    return render_template('admin.html', attacker=attacker, admin_path=admin_path)
 
 @app.route('/', defaults={'path': ''}, methods=HTTP_METHODS)
 @app.route('/<path:path>', methods=HTTP_METHODS)
 def catch_all(path):
-    body = "configure the default attacker using the <a href='/admin'>admin</a> URL or a single-path attacker specifing a <i>path</i> after the admin URL (e.g., the <a href='/admin/example.html'>example.html</a> page attacker)"
+    if path:
+        body = "configure the default attacker using the <a href='" + admin_path + \
+               "'>admin</a> URL or a <a href='" + admin_path + "/" + path + "'>single-path</a> attacker."
+    else:
+        body = "configure the default attacker using the <a href='" + admin_path + \
+               "'>admin</a> URL or a <a href='" + admin_path +  "/example.html'>single-path</a> attacker."
     code = "200"
     message = "OK"
 
     attacker = loadAttacker(request.path[1:])
-
-    '''
-    if path:
-        fname = "json/" + base64.b64encode(path.encode('ascii')).decode('ascii') + ".json"
-    else:
-        fname = None
-
-    try:
-        with open(fname, "r") as f:
-            attacker = json.loads(f.read())
-    except:
-        try:
-            with open("attacker.json", "r") as f:
-                attacker = json.loads(f.read())
-        except:
-            attacker = {}
-    '''
 
     if 'body' in attacker:
         body = attacker['body']
